8261a70c66915e9238b536e1977d8c36394f5edc
ArijitChakr <arijit.chakraborty2304@gmail.com>
2024-10-15 20:24:58 +0530

    Adding project file

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+app.get("/add", function (req, res) {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+  res.json({
+    ans: a + b,
+  });
+});
+
+app.get("/multiply", function (req, res) {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+  res.json({
+    ans: a * b,
+  });
+});
+
+app.get("/divide", function (req, res) {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+  res.json({
+    ans: a / b,
+  });
+});
+
+app.get("/subtract", function (req, res) {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+  res.json({
+    ans: a - b,
+  });
+});
+
+app.listen(3000);
